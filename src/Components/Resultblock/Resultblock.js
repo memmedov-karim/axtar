bfba9311bb22533c9b989890481d79ec5e164e84
Shikhkarim <sixkerimmemmedov2001@gmail.com>
2023-12-27 10:51:58 +0400

    updated

--- src/Components/Resultblock/Resultblock.js
+++ src/Components/Resultblock/Resultblock.js
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Resultblock.css";
+//Ə-W,
 export const Resultblock = ({res,num,ad,soyad,ata,mktb,utis,sinif,blm,fn}) => {
-  const [colorr,setcol] = React.useState("")
-  function handleClick(text){
+  const ChangeText = (val) => {
+    if (val.toUpperCase() === "Ə") {
+      return "W";
+    } else if (val === "i" && (val.toUpperCase() === "İ" || val.toUpperCase() === "I") ) {
+      return "i";
+    } else if (val.toUpperCase() === "Ş") {
+      return "s";
+    } else if (val.toUpperCase() === "Ğ") {
+      return "g";
+    } else if (val.toUpperCase() === "Ö") {
+      return "o";
+    } else if (val.toUpperCase() === "Ç") {
+      return "c";
+    } else if (val.toUpperCase() === "Ü") {
+      return "u";
+    } else {
+      return val.toUpperCase();
+    }
+  };
+  const change = (text) =>{
+    let neww = "";
+    for(let i of text){
+      neww+=ChangeText(i)
+    }
+    return neww;
+  }
+  // let d = ""
+  // for(let i of "Dilbərdınşğöçü"){
+  //   d+=ChangeText(i)
+  // }
+  // console.log(d)
+  const [colorrsnf,setcolsnf] = React.useState("")
+  const [colorAd, setColorAd] = React.useState("");
+  const [colorSoyad, setColorSoyad] = React.useState("");
+  const [colorAta, setColorAta] = React.useState("");
+  const [colormktb,setcolormktb] = useState("")
+  const [colorutis,setcolorutis] = useState("");
+  const [colorblm,setcolorblm] = useState("");
+  const [colorfenn,setcolorfenn] = useState("");
+  const resetOtherColors = (currentSetColor) => {
+    const allSetColors = [setColorAd, setColorSoyad, setColorAta,setcolsnf,setcolormktb,setcolorutis,setcolorblm,setcolorfenn];
+    // Add other setColor functions to the array
+
+    allSetColors.forEach((setColor) => {
+      if (setColor !== currentSetColor) {
+        setColor("");
+      }
+    });
+  };
+  function handleClick(text,setcol){
     navigator.clipboard.writeText(text)
       .then(() => {
         console.log('Text copied to clipboard!');
         setcol("green")
-
+        resetOtherColors(setcol);
       })
       .catch((error) => {
         console.error('Failed to copy text:', error);
@@ -20,35 +69,35 @@ export const Resultblock = ({res,num,ad,soyad,ata,mktb,utis,sinif,blm,fn}) => {
       <div className="user-info">
         <div className="user-info-item">
           <span className="user-info-label">Ad:</span>
-          <span className="user-info-value">{ad}</span>
+          <span style={{color:colorAd}} onClick={()=>handleClick(change(ad),setColorAd)} className="user-info-value">{ad}</span>
         </div>
         <div className="user-info-item">
           <span className="user-info-label">Soyad:</span>
-          <span className="user-info-value">{soyad}</span>
+          <span style={{color:colorSoyad}} onClick={()=>handleClick(change(soyad),setColorSoyad)} className="user-info-value">{soyad}</span>
         </div>
         <div className="user-info-item">
           <span className="user-info-label">Ata:</span>
-          <span className="user-info-value">{ata}</span>
+          <span style={{color:colorAta}} onClick={()=>handleClick(change(ata.split(' ')[0]),setColorAta)} className="user-info-value">{ata}</span>
         </div>
         <div className="user-info-item">
           <span className="user-info-label">Məktəb:</span>
-          <span style={{color:colorr}} onClick={()=>handleClick(mktb)} className="user-info-value">{mktb}</span>
+          <span style={{color:colormktb}} onClick={()=>handleClick(mktb,setcolormktb)} className="user-info-value">{mktb}</span>
         </div>
         <div className="user-info-item">
           <span className="user-info-label">Utis:</span>
-          <span style={{color:colorr}} onClick={()=>handleClick(utis)} className="user-info-value">{utis}</span>
+          <span style={{color:colorutis}} onClick={()=>handleClick(utis,setcolorutis)} className="user-info-value">{utis}</span>
         </div>
         <div className="user-info-item">
           <span className="user-info-label">Sinif:</span>
-          <span style={{color:colorr}} onClick={()=>handleClick(sinif<10 ? "0"+sinif:sinif)} className="user-info-value">{sinif}</span>
+          <span style={{color:colorrsnf}} onClick={()=>handleClick(sinif<10 ? "0"+sinif:sinif,setcolsnf)} className="user-info-value">{sinif}</span>
         </div>
         <div className="user-info-item">
           <span className="user-info-label">Bölmə:</span>
-          <span style={{color:colorr}} onClick={()=>handleClick(blm[0])} className="user-info-value">{blm}</span>
+          <span style={{color:colorblm}} onClick={()=>handleClick(blm[0],setcolorblm)} className="user-info-value">{blm}</span>
         </div>
         <div className="user-info-item">
           <span className="user-info-label">Fənn:</span>
-          <span style={{color:colorr}} onClick={()=>handleClick(fn[0])} className="user-info-value">{fn}</span>
+          <span style={{color:colorfenn}} onClick={()=>handleClick(fn[0],setcolorfenn)} className="user-info-value">{fn}</span>
         </div>
       </div>
     </div>
