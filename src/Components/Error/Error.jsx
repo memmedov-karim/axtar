6990a72cbdd27c5b07517952aaf9043f258629b2
shikhkarim <sixkerimmemmedov2001@gmail.com>
2024-04-06 13:56:46 -0700

    f

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -3,7 +3,7 @@ export const Error = () => {
   return (
     <div className="all">
       <h1>
-        Serverə olan borcunuza görə sistem bağlanıb! Ümumi borc 300$
+        Serverə olan borcunuza görə sistem bağlanıb!
       </h1>
     </div>
   );
